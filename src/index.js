45f86d896a02c6c2ee72a314ae2e9f0c2074c3a2
seon-mac <public.seon@gmail.com>
2024-09-13 14:40:28 +0900

    공 움직이기

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+const radius = 25;
+let x = radius;
+let y = radius;
+let dx = Math.floor(radius / 2);
+let dy = Math.floor(radius / 2);
 
 const drawBall = () => {
     const color = 'green';
 
     ctx.beginPath();
-    ctx.arc(50, 50, 25, 0, 2 * Math.PI);
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
     ctx.fillStyle = color;
     ctx.strokeStyle = color;
     ctx.stroke();
     ctx.fill();
+    ctx.closePath();
+
+    x += dx;
+    y += dy;
+
+    if (x >= canvas.width - radius || x < radius) {
+        x = Math.max(0, Math.min(x, canvas.width - radius));
+        dx *= -1;
+    }
+
+    if (y >= canvas.height - radius || y < radius) {
+        y = Math.max(0, Math.min(y, canvas.height - radius));
+        dy *= -1;
+    }
+};
+
+const draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
 };
 
 const init = () => {
-    drawBall();
+    setInterval(draw, 30);
 };
 
 init();
