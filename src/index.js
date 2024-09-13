fd6d601ffe815fef1271263effc45beb54fe1772
seon-mac <public.seon@gmail.com>
2024-09-13 13:47:45 +0900

    원 그리기 함수 추가

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const drawBall = () => {
+    const color = 'green';
+
+    ctx.beginPath();
+    ctx.arc(50, 50, 25, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.fill();
+};
+
+const init = () => {
+    drawBall();
+};
+
+init();
